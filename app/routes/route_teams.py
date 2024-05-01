3c6077e3bd904e1522dcf2502116d29f3fc4d3db
marcelosanchez166 <marcelosanchez166@gmail.com>
2024-04-30 23:01:21 -0600

    Se agrego validacion para que la hora inicio sea menor a la fin

--- app/routes/route_teams.py
+++ app/routes/route_teams.py
@@ -32,23 +32,67 @@ def equipos():
             horas_por_dia = {}
             for i in dia_lunes:
                 print(i, 'Valor de la lista que se obtiene del formulario')
-            print(type(dia_lunes))
+            # print(type(dia_lunes))
             for dia in dia_lunes:
                 inicio = request.form['horas[{}][inicio]'.format(dia)]
                 fin = request.form['horas[{}][fin]'.format(dia)]
-                horas_por_dia[dia] = {'inicio': inicio, 'fin': fin}
+                if  inicio < fin:
+                    horas_por_dia[dia] = {dia:{'inicio': inicio, 'fin': fin}}
+                else:
+                    flash('La hora final debe ser distinta a la inicial', 'error')
+                    return redirect(url_for( "EquiposBlueprint.equipos" ))
                 print("Imprimiendo el dia y las horas ",dia,horas_por_dia[dia])
 
+
             dia_martes = request.form.getlist( 'dias[martes]' )
-            horas_por_dia = {}
-            for i in dia_martes:
-                print(i, 'Valor de la lista que se obtiene del formulario')
-            print(type(dia_martes))
             for dia in dia_martes:
                 inicio = request.form['horas[{}][inicio]'.format(dia)]
                 fin = request.form['horas[{}][fin]'.format(dia)]
-                horas_por_dia[dia] = {'inicio': inicio, 'fin': fin}
+                if  inicio < fin:
+                    horas_por_dia[dia] = {dia :{'inicio': inicio, 'fin': fin}}
+                else:
+                    flash('La hora final debe ser distinta a la inicial', 'error')
+                    return redirect(url_for( "EquiposBlueprint.equipos" ))
                 print("Imprimiendo el dia y las horas ",dia,horas_por_dia[dia])
+
+
+            dia_miercoles = request.form.getlist('dias[miercoles]')
+            for dia in dia_miercoles:
+                inicio =request.form['horas[{}][inicio]'.format(dia)]
+                fin =  request.form['horas[{}][fin]'.format(dia) ]
+                if  inicio < fin:
+                    horas_por_dia[dia] = {dia :{'inicio': inicio, 'fin': fin}}
+                else:
+                    flash('La hora final debe ser distinta a la inicial', 'error')
+                    return redirect(url_for( "EquiposBlueprint.equipos" ))
+                print("Imprimiendo el dia y las horas ",dia,horas_por_dia[dia])
+
+
+            dia_jueves= request.form.getlist('dias[jueves]')
+            for dia in dia_jueves:
+                inicio = request.form['horas[{}][inicio]'.format((dia))]
+                fin = request.form['horas[{}][fin]'.format(dia)]
+                if  inicio < fin:
+                    horas_por_dia[dia] = {dia :{'inicio': inicio, 'fin': fin}}
+                else:
+                    flash('La hora final debe ser distinta a la inicial', 'error')
+                    return redirect(url_for( "EquiposBlueprint.equipos" ))
+                print("Imprimiendo el dia y las horas ",dia,horas_por_dia[dia])
+
+
+            dia_viernes = request.form.getlist('dias[viernes]')
+            for dia in dia_viernes:
+                inicio =request.form['horas[{}][inicio]'.format(dia)]
+                fin = request.form['horas[{}][fin]'.format(dia)]
+                if inicio < fin:
+                    horas_por_dia[dia] = {dia :{'inicio': inicio, 'fin': fin}}
+                else:
+                    flash('La hora final debe ser distinta a la inicial', 'error')
+                    return redirect(url_for( "EquiposBlueprint.equipos" ))
+                print("Imprimiendo el dia y las horas virnesssss ",dia,horas_por_dia[dia])
+
+            print(horas_por_dia, "diccionario de los dias y horas ")
+
 
             Add_Equipos = Equipos(None,nombre_equipo, representante, subrepresentante, correo,  None)
             print("DEsde la instancia de Equipos",Add_Equipos.correo, Add_Equipos.nombre_equipo, Add_Equipos.representante, Add_Equipos.subrepresentante)
@@ -69,14 +113,16 @@ def equipos():
     else:
         flash('You must be logged in to access this page', 'warning')
         return redirect(url_for('AuthLogin.login'))  # Redirecciona a la página de inicio de sesión
-    
+
+"""Arreglar que cuando la hora inicio sea mayor a la hora fin que no se agregue el equipo Puede que en lugar de un redirect mejor usar un render_template"""
+
 
 @EquiposBlueprint.route("/edit_equipo/<int:id>", methods=["POST", "GET"])
-def  edit_equipo():
+def  edit_equipo(id):
     if current_user.is_authenticated:
         connection= get_connection()
         with connection.cursor() as cursor:
-            cursor.execute("SELECT id, nombre_equipo, representante, subrepresentante, correo, grupo_id FROM equipos ")
+            cursor.execute("SELECT id, nombre_equipo, representante, subrepresentante, correo, grupo_id FROM equipos  WHERE id= %s", (id) )
             data = cursor.fetchone()#fetchone() devuelve una sola fila (la primera fila que cumple con la condición) o None si no hay ninguna fila que coincida.
             print(data[0], data[1], data, "Data obtenida del select de edit_team")
         if request.method == "POST":
@@ -89,15 +135,16 @@ def  edit_equipo():
                 print("Entro al post desde el formulario el nombre nuevo de la tarea", nombre_equipo, representante, subrepresentante, correo, grupo_id)
                 update = Equipos(data[0], nombre_equipo, representante, subrepresentante, correo, grupo_id)
                 print(update.id, update.nombre_equipo, update.representante, update.subrepresentante, update.correo, update.grupo_id, "lo que se le envia a la clase Equipos \n")
-            try:
-                update_equipos = ModeloEquipos.update_equipos(update) 
-                if update_equipos is  not None:#Pregunto si la instancia del metodo de clase es diferente de None entonces que envie un mensaje flash diciendo que se elimino la tarea ya que el proceso de eliminacion se hara en el metodo de instancia de  ModeloTareas.delete_tarea
-                    flash("Equipos Actualizado correctamente", "success")
-                    return redirect(url_for("EquiposBlueprint.equipos"))
-                else:
-                    flash("Error actualizando el equipo", "warning")#Si la eliminacion de la tarea en el metodo de clase  ModeloTareas.delete_tarea falla se mostrara este msj
-            except  Exception as ex:
-                print ("Error al actualizar los datos del equipo",ex)
+                try:
+                    update_equipos = ModeloEquipos.update_equipos(update) 
+                    if update_equipos is  not None:#Pregunto si la instancia del metodo de clase es diferente de None entonces que envie un mensaje flash diciendo que se elimino la tarea ya que el proceso de eliminacion se hara en el metodo de instancia de  ModeloTareas.delete_tarea
+                        flash("Equipos Actualizado correctamente", "success")
+                        return redirect(url_for("EquiposBlueprint.equipos"))
+                    else:
+                        flash("Error actualizando el equipo", "warning")#Si la eliminacion de la tarea en el metodo de clase  ModeloTareas.delete_tarea falla se mostrara este msj
+                except  Exception as ex:
+                    flash("Error al actualizar los datos del equipo", "warning")
+                    print ("Error al actualizar los datos del equipo",ex)
         return render_template("edit_equipos.html")
         # Obtener todas las tareas asociadas al usuario después de eliminar la tarea en el metodo de clase  ModeloTareas.delete_tarea, esta nueva lista de tareas se envia con la renderizacion de la plantilla task.html de la siguiente forma return render_template("task.html", send_tasks=send_tasks)
     connection= get_connection()
