223284dbbc870d117683b5fa741b465cf5042b93
marcelosanchez166 <marcelosanchez166@gmail.com>
2024-09-15 22:41:27 -0600

    Definiendo los equipos en la tabla para que el grupo aparezca una sola vez

--- app/models/modelo_grupos.py
+++ app/models/modelo_grupos.py
@@ -2,10 +2,20 @@ from app.models.entities.grupos import Grupos
 from flask import flash, render_template
 from app.database.db import get_connection
 
-# class ModeloGrupos:
-#     @classmethod
-#     def get_grupos(cls):
-#         try:
-#             connection = get_connection()
-#             cursor = connection.cursor()
-#             cursor.execute('''
+class ModeloGrupos:
+    @classmethod
+    def crear_grupos(self, equipo, grupo):
+        try:
+            connection= get_connection()
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT id, nombre FROM grupos WHERE nombre = %s ", (grupo,))
+                data = cursor.fetchone()#fetchone() devuelve una sola fila (la primera fila que cumple con la condición) o None si no hay ninguna fila que coincida.
+                #fectchone es para verificar si almenos una linea completa de la tabla coincide y fetchall busca coincidencias en todas las lineas de la tabla
+                idgrupo=data[0]
+                print(idgrupo)
+                cursor.execute("UPDATE `equipos` SET `grupo_id` = %s  WHERE `equipos`.`nombre_equipo` = %s ", (idgrupo, equipo,))
+                connection.commit()
+                return True
+        except Exception as e:
+            print(e)
+            flash("No se pudo agregar el equipo al grupo")
--- app/routes/route_groups.py
+++ app/routes/route_groups.py
@@ -3,6 +3,7 @@ from flask_login import LoginManager,  login_user, login_required, logout_user,c
 
 # from app import current_user,login_required
 from app.database.db import get_connection
+from app.models.modelo_grupos import ModeloGrupos
 
 
 gruposBlueprint=Blueprint("gruposBlueprint", __name__)
@@ -37,16 +38,28 @@ def grupos():
                 equipo = request.form.get("equipo")
                 grupo = request.form.get("grupo")
                 print(equipo, grupo)
-                return redirect(url_for('gruposBlueprint.grupos'))
+                Egrupos = ModeloGrupos.crear_grupos(equipo, grupo)
+                if Egrupos:
+                    flash("Equipo agregado al grupo exitosamente", "success")
+                    return redirect(url_for('gruposBlueprint.grupos'))
             else:
                 flash("Error en los datos enviador", "warning")
-                
-        return render_template("grupos.html", grupos=grupos, dates=horarios, equipos=equipos)
+            cursor.execute("""SELECT  grupos.id, grupos.nombre AS grupo, GROUP_CONCAT(equipos.nombre_equipo ORDER BY equipos.nombre_equipo SEPARATOR ', ') AS equipos
+                        FROM equipos JOIN grupos ON equipos.grupo_id = grupos.id 
+                        GROUP BY grupos.nombre 
+                        ORDER BY grupos.nombre;
+                        """)
+            GruposE = cursor.fetchall()
+            grupos_creados = [{'grupo_id' : row[0],'nombre_equipo': row[1], 'nombre': row[2]} for row in GruposE]
+            print(grupos_creados[0])
+        return render_template("grupos.html", grupos=grupos, dates=horarios, equipos=equipos, grupos_creados=grupos_creados)
     else:
         """Redirección a la página principal con un mensaje de error"""
         return render_template('auth/auth.html')
 
 
+#Aqui debo hacer que me redirija hacia una pantalla de editar para actualizar el id del grupo, Aqui tambien debo enviar la data a un metodo en el archivo modelo_grupos para que 
+#Haga el trabajo de Actualizar el grupo en la tabla equipos
 @login_required
 @gruposBlueprint.route("/edit_grupos/<int:grupo_id>", methods=["GET", "POST"])
 def edit_grupos(grupo_id):
@@ -66,3 +79,14 @@ def edit_grupos(grupo_id):
         return render_template('auth/auth.html')
     
 
+#El DELETE debo hacerlo como un UPDATE para que no se elimine como tal el grupo, y ademas debo enviar la data a algun metodo en el archivo modelo_grupos para que haga el trabajo
+#ELIMAR(Actualizar) el grupo en la tabla equipos
+@login_required
+@gruposBlueprint.route("/delete_grupos/<int:id_equipos>", methods=["GET","POST"])
+def delete_grupos(id_equipos):
+    if current_user.is_authenticated:
+        connection = get_connection()
+        with connection.cursor() as cursor:
+            cursor.execute("UPDATE `equipos` SET `grupo_id` = %s  WHERE `equipos`.`id` = %s ", ('NULL', id_equipos,))
+            connection.commit()
+            return redirect(url_for("gruposBlueprint.grupos"))
