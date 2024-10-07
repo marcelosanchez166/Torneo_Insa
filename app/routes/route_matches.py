618ecfb6db1c3545558fe2edad50be32094bfa50
marcelosanchez166 <marcelosanchez166@gmail.com>
2024-10-06 22:00:13 -0600

    Creando formulario y tablas para cada grupo de esta forma poder crear los partidos por cada tabla de grupo

--- app/routes/route_matches.py
+++ app/routes/route_matches.py
@@ -25,11 +25,21 @@ def partidos():
                 ORDER BY MIN(TIMESTAMP(horarios.dia, horarios.hora_inicio));
             """)
             data2 = cursor.fetchall()
-            cursor.execute("""SELECT COUNT(DISTINCT grupo_id) FROM equipos;""")
-            conteo_grupo = cursor.fetchone()[0]  # Obtener el primer valor de la tupla
+            print(type(data2))
+            cursor.execute("""SELECT e.grupo_id, g.nombre AS nombre_grupo, (SELECT COUNT(DISTINCT grupo_id) FROM equipos)
+                        AS total_grupos, e.id AS equipo_id, e.nombre_equipo FROM equipos e 
+                        JOIN grupos g ON e.grupo_id = g.id 
+                        ORDER BY e.grupo_id, e.nombre_equipo;""")
+            conteo_grupo = cursor.fetchall()  # Obtener los valores
+            count = 0
+            grupos = []  # Crear lista vacía
+            for row in conteo_grupo:
+                count += row[0]  # Sumar el total de grupos
+                grupos.append({'grupo_id': row[0],'nombre_grupo': row[1], 'total_grupos': row[2], 'equipo_id':row[3], 'nombre_equipo':row[4], 'count': count})  # Agregar cada grupo a la lista
+            print(grupos)
             #Ajustar la creación de `horarios` para reflejar los datos obtenidos de la consulta
             horarios = [{'nombre_equipo': row[0], 'horarios': row[1]} for row in data2]
-            return render_template("partidos.html",  dates=horarios, conteo_grupo=conteo_grupo)
+            return render_template("partidos.html",  dates=horarios, grupos=grupos)
     else:
         """Redirección a la página principal con un mensaje de error"""
         return render_template('auth/auth.html')
